28c7a877c3cc28a0fcdd53324c3f2431b3b997e0
AneeshBoreda <aneesh.boreda92@gmail.com>
2018-11-25 21:56:22 -0500

    added model

--- neuralnet.py
+++ neuralnet.py
@@ -1,194 +1,156 @@
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-"""Convolutional Neural Network Estimator for MNIST, built with tf.layers."""
+
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+import time
 
 import numpy as np
 import tensorflow as tf
+import tensorflow.contrib.slim as slim
+class Model:
+    def __init__(self, data_x, data_y):
+        self.n_class = 7
+        self._create_architecture(data_x, data_y)
 
-tf.logging.set_verbosity(tf.logging.INFO)
+    def _create_architecture(self, data_X, data_y):
+        y_hot = tf.one_hot(data_y, depth = self.n_class)
+        logits = self._create_model(data_X)
+        predictions = tf.argmax(logits, 1, output_type = tf.int32)
+        self.loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits_v2(labels = y_hot,
+                                                                              logits = logits))
+        self.optimizer = tf.train.AdamOptimizer(learning_rate = 0.001).minimize(self.loss)
+        self.accuracy = tf.reduce_sum(tf.cast(tf.equal(predictions, data_y), tf.float32))
+
+    def _create_model(self, X):
+        X1 = X - 0.5
+        X1 = tf.pad(X1, tf.constant([[0, 0], [2, 2], [2, 2], [0, 0]]))
+        with slim.arg_scope([slim.conv2d, slim.fully_connected],
+                            weights_initializer = tf.truncated_normal_initializer(0.0, 0.1)):
+            net = slim.conv2d(X1, 6, [5, 5], padding = 'VALID')
+            net = slim.max_pool2d(net, [2, 2])
+            net = slim.conv2d(net, 16, [5, 5], padding = 'VALID')
+            net = slim.max_pool2d(net, [2, 2])
+
+            net = tf.reshape(net, [-1, 400])
+            net = slim.fully_connected(net, 120)
+            net = slim.fully_connected(net, 84)
+            net = slim.fully_connected(net, self.n_class, activation_fn = None)
+        return net
 
 
-def cnn_model_fn(features, labels, mode):
-	"""Model function for CNN."""
-	# Input Layer
-	# Reshape X to 4-D tensor: [batch_size, width, height, channels]
-	# MNIST images are 28x28 pixels, and have one color channel
-	input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
 
-	# Convolutional Layer #1
-	# Computes 32 features using a 5x5 filter with ReLU activation.
-	# Padding is added to preserve width and height.
-	# Input Tensor Shape: [batch_size, 28, 28, 1]
-	# Output Tensor Shape: [batch_size, 28, 28, 32]
-	conv1 = tf.layers.conv2d(
-	    inputs=input_layer,
-	    filters=32,
-	    kernel_size=[5, 5],
-	    padding="same",
-	    activation=tf.nn.relu)
 
-	# Pooling Layer #1
-	# First max pooling layer with a 2x2 filter and stride of 2
-	# Input Tensor Shape: [batch_size, 28, 28, 32]
-	# Output Tensor Shape: [batch_size, 14, 14, 32]
-	pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+def getLabels(x,image_to_label,labeldict):
 
-	# Convolutional Layer #2
-	# Computes 64 features using a 5x5 filter.
-	# Padding is added to preserve width and height.
-	# Input Tensor Shape: [batch_size, 14, 14, 32]
-	# Output Tensor Shape: [batch_size, 14, 14, 64]
-	conv2 = tf.layers.conv2d(
-	    inputs=pool1,
-	    filters=64,
-	    kernel_size=[5, 5],
-	    padding="same",
-	    activation=tf.nn.relu)
-
-	# Pooling Layer #2
-	# Second max pooling layer with a 2x2 filter and stride of 2
-	# Input Tensor Shape: [batch_size, 14, 14, 64]
-	# Output Tensor Shape: [batch_size, 7, 7, 64]
-	pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
-
-	# Flatten tensor into a batch of vectors
-	# Input Tensor Shape: [batch_size, 7, 7, 64]
-	# Output Tensor Shape: [batch_size, 7 * 7 * 64]
-	pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
-
-	# Dense Layer
-	# Densely connected layer with 1024 neurons
-	# Input Tensor Shape: [batch_size, 7 * 7 * 64]
-	# Output Tensor Shape: [batch_size, 1024]
-	dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
-
-	# Add dropout operation; 0.6 probability that element will be kept
-	dropout = tf.layers.dropout(
-	    inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
-
-	# Logits layer
-	# Input Tensor Shape: [batch_size, 1024]
-	# Output Tensor Shape: [batch_size, 10]
-	logits = tf.layers.dense(inputs=dropout, units=10)
-
-	predictions = {
-	    # Generate predictions (for PREDICT and EVAL mode)
-	    "classes": tf.argmax(input=logits, axis=1),
-	    # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
-	    # `logging_hook`.
-	    "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
-	}
-	if mode == tf.estimator.ModeKeys.PREDICT:
-	  return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
-
-	 # Calculate Loss (for both TRAIN and EVAL modes)
-	loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-
-	 # Configure the Training Op (for TRAIN mode)
-	if mode == tf.estimator.ModeKeys.TRAIN:
-	   optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-	   train_op = optimizer.minimize(
-	      loss=loss,
-	      global_step=tf.train.get_global_step())
-	   return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-
-	 # Add evaluation metrics (for EVAL mode)
-	eval_metric_ops = {
-	     "accuracy": tf.metrics.accuracy(
-	         labels=labels, predictions=predictions["classes"])}
-	return tf.estimator.EstimatorSpec(
-	     mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
-
+    image_to_label['./Images/'+x[0].strip()+'.jpg']=labeldict[x[1].strip()]
+    return x
 def main(unused_argv):
-	 # Load training and eval data
-	# mnist = tf.contrib.learn.datasets.load_dataset("mnist")
-	# A vector of filenames.
-	dir_path = "./Images/"
-	pictures = tf.gfile.ListDirectory(dir_path)
-	#print(pictures)
-	#exit(0)
-	pictures = ['./Images/'+x.strip() for x in pictures]
-	filenames = tf.constant(pictures)
-	#print(filenames)
-	print(tf.shape(filenames))
+   labeldict={}
+   X=tf.placeholder(tf.float32)
+   Y=tf.placeholder(tf.float32)
 
-	# `labels[i]` is the label for the image in `filenames[i].
-	labels = tf.constant([i for i in range(10015)])
+   labeldict['nv']=0
+   labeldict['mel']=1
+   labeldict['bkl']=2
+   labeldict['bcc']=3
+   labeldict['akiec']=4
+   labeldict['vasc']=5
+   labeldict['df']=6
+   image_to_label={}
+   #tf.enable_eager_execution()
+   # Load training and eval data
+   # mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+   # A vector of filenames.
+   dir_path = "./Images/"
+   pictures = tf.gfile.ListDirectory(dir_path)
+   #print(pictures)
+   #exit(0)
+   pictures = [dir_path+x.strip() for x in pictures]
+   filenames = tf.constant(pictures)
+   #print(filenames)
+   meta_data=np.genfromtxt('HAM10000_metadata.csv',delimiter=',',usecols=(1,2),dtype=np.unicode_,skip_header=1)
+   np.apply_along_axis(func1d=getLabels,axis=1,arr=meta_data,image_to_label=image_to_label,labeldict=labeldict)
+   # `labels[i]` is the label for the image in `filenames[i].
+   labels = tf.constant([image_to_label[i] for i in pictures])
 
-	dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
-	dataset = dataset.map(_parse_function)
-	dataset.batch(batch_size = 10)
-	iter = dataset.make_one_shot_iterator()
-	record_defaults = [tf.float32] * 2
-	meta_data = tf.contrib.data.CsvDataset(["HAM10000_metadata.csv"], record_defaults, select_cols = [2, 3], header = True)
-	print(meta_data)
-	iter2 = meta_data.make_one_shot_iterator()
-	next_batch = iter.get_next()
-	with tf.Session() as sess:
-	   sess.run(tf.global_variables_initializer())
-	   try:
-	        while True:
-	            sess.run(next_batch)
-	   except tf.errors.OutOfRangeError:
-	       pass
+   dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
+
+   dataset = dataset.map(_parse_function)
+   dataset.batch(batch_size = 10)
+   iter = dataset.make_one_shot_iterator()
+   record_defaults = [tf.float32] * 2
+
+   #meta_data = tf.contrib.data.CsvDataset(["HAM10000_metadata.csv"], record_defaults, select_cols = [2, 3], header = True)
+
+
+
+  # for x,y in meta_data:
+    #   print(x,y)
+#   print(meta_data[1])
+   #iter2 = meta_data.make_one_shot_iterator()
+#   for x,y in iter2:
+#      print(x,y)
+   data_X,data_Y = iter.get_next()
+   with tf.Session() as sess:
+      start=time.time()
+      sess.run(tf.global_variables_initializer())
+      try:
+           c=0
+           while True:
+               c+=1
+               sess.run({X:data_X,Y:data_Y})
+               if(c%1000==0):
+                   print('Current Time:',time.time()-start)
+                   print('Iteration:',c)
+                   #print('Values:',data_Y)
+      except tf.errors.OutOfRangeError:
+          print('Done')
 # Reads an image from a file, decodes it into a dense tensor, and resizes it
 # to a fixed shape.
 def _parse_function(filename, label):
-	 image_string = tf.read_file(filename)
-	 image_decoded = tf.image.decode_jpeg(image_string)
-	 image_resized = tf.image.resize_images(image_decoded, [28, 28])
-	 return image_resized, label
+    image_string = tf.read_file(filename)
+    image_decoded = tf.image.decode_jpeg(image_string)
+    image_resized = tf.image.resize_images(image_decoded, [28, 28])
+    return image_resized, label
 
 """train_data = mnist.train.images  # Returns np.array
-	train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
-	eval_data = mnist.test.images  # Returns np.array
-	eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+   train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+   eval_data = mnist.test.images  # Returns np.array
+   eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
 
-	 # Create the Estimator
-	mnist_classifier = tf.estimator.Estimator(
-	   model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model")
+    # Create the Estimator
+   mnist_classifier = tf.estimator.Estimator(
+      model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model")
 
-	 # Set up logging for predictions
-	 # Log the values in the "Softmax" tensor with label "probabilities"
-	 tensors_to_log = {"probabilities": "softmax_tensor"}
-	 logging_hook = tf.train.LoggingTensorHook(
-	     tensors=tensors_to_log, every_n_iter=50)
+    # Set up logging for predictions
+    # Log the values in the "Softmax" tensor with label "probabilities"
+    tensors_to_log = {"probabilities": "softmax_tensor"}
+    logging_hook = tf.train.LoggingTensorHook(
+        tensors=tensors_to_log, every_n_iter=50)
 
-	 # Train the model
-	 train_input_fn = tf.estimator.inputs.numpy_input_fn(
-	     x={"x": train_data},
-	     y=train_labels,
-	     batch_size=100,
-	     num_epochs=None,
-	     shuffle=True)
-	 mnist_classifier.train(
-	     input_fn=train_input_fn,
-	     steps=10,
-	     hooks=[logging_hook])
+    # Train the model
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": train_data},
+        y=train_labels,
+        batch_size=100,
+        num_epochs=None,
+        shuffle=True)
+    mnist_classifier.train(
+        input_fn=train_input_fn,
+        steps=10,
+        hooks=[logging_hook])
 
-	 # Evaluate the model and print results
-	 eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-	     x={"x": eval_data},
-	     y=eval_labels,
-	     num_epochs=1,
-	     shuffle=False)
-	 eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
-	 print(eval_results)"""
+    # Evaluate the model and print results
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": eval_data},
+        y=eval_labels,
+        num_epochs=1,
+        shuffle=False)
+    eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+    print(eval_results)"""
 
 
 
 if __name__ == "__main__":
-	 tf.app.run()
+    tf.app.run()
